299b6281aab0871e41a9fd86212eea4b7e292d3a
Edward <edward.arevalo@sipse.com.co>
2021-12-09 20:50:58 -0500

    actualización de sistema

--- database/migrations/2021_11_27_113309_create_parametrics_table.php
+++ database/migrations/2021_11_27_113309_create_parametrics_table.php
@@ -13,18 +13,20 @@ class CreateParametricsTable extends Migration
      */
     public function up()
     {
-        Schema::create('parametrics', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('p_category',100);
-            $table->string('p_value',100);
-            $table->string('p_text',255);
-            $table->integer('p_order');
-            $table->timestamps();
-            $table->softDeletes('deleted_at');
-            $table->integer('created_by')->nullable()->unsigned();
-            $table->integer('updated_by')->nullable()->unsigned();
-            $table->integer('deleted_by')->nullable()->unsigned();
-        });
+        if(!Schema::hasTable('table')) {
+            Schema::create('parametrics', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('p_category',100);
+                $table->string('p_value',100);
+                $table->string('p_text',255);
+                $table->integer('p_order');
+                $table->timestamps();
+                $table->softDeletes('deleted_at');
+                $table->integer('created_by')->nullable()->unsigned();
+                $table->integer('updated_by')->nullable()->unsigned();
+                $table->integer('deleted_by')->nullable()->unsigned();
+            });
+        }
     }
 
     /**
