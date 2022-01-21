c37da4c47646d8a9259e34402edcc33dfd6a2f48
hansselhurtado <hansselhurtado@gmail.com>
2022-01-21 18:17:06 -0500

    se agrega migracion para soportar modulo de solicitud

---
+++ database/migrations/2022_01_21_172541_create_request_tables.php
@@ -0,0 +1,186 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_states', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date_delivery')->comment('fecha de entrega');
+            $table->string('observation')->nullable();
+
+            $table->unsignedInteger('type_service_id')->comment('tipo de servicio');
+            $table->foreign('type_service_id')->references('id')->on('parametrics');
+
+            $table->unsignedBigInteger('request_state_id')->comment('estado de solicitud');
+            $table->foreign('request_state_id')->references('id')->on('request_states');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('file');
+
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('requests');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('start_date')->comment('fecha de inicio');
+            $table->dateTime('ending_date')->comment('fecha fin');
+
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('requests');
+
+            $table->unsignedBigInteger('request_state_id');
+            $table->foreign('request_state_id')->references('id')->on('request_states');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('question');
+            $table->enum('question_type',['ABIERTA','CERRADA'])->comment('tipo de pregunta');
+
+            $table->unsignedInteger('type_service_id')->comment('tipo de servicio');
+            $table->foreign('type_service_id')->references('id')->on('parametrics');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_responses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('response')->comment('respuesta');
+
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('requests');
+
+            $table->unsignedBigInteger('request_question_id');
+            $table->foreign('request_question_id')->references('id')->on('request_questions');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_themes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('theme_id')->comment('id de la relacion, topic, language, system');
+            $table->enum('category',['TOPIC','SYSTEM','LANGUAGE'])->comment('categoria de solicitud');
+
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('requests');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_quote_tutors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('value')->comment('valor de la cotización del tutor');
+            $table->string('observation')->nullable();
+
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('requests');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('request_quotes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('value')->comment('valor de la cotización');
+            $table->string('observation')->nullable();
+
+            $table->unsignedBigInteger('request_quote_tutor_id');
+            $table->foreign('request_quote_tutor_id')->references('id')->on('request_quote_tutors');
+
+            $table->enum('status',['ACTIVO','DESACTIVADO'])->default('ACTIVO');
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_states');
+        Schema::dropIfExists('requests');
+        Schema::dropIfExists('request_files');
+        Schema::dropIfExists('request_history');
+        Schema::dropIfExists('request_questions');
+        Schema::dropIfExists('request_responses');
+        Schema::dropIfExists('request_themes');
+        Schema::dropIfExists('request_quote_tutors');
+        Schema::dropIfExists('request_quotes');
+    }
+}
