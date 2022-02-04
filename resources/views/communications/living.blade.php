1694da6d8d9218a67de976aebf254cb3e795e3d5
camilo300792 <eliascamiloms92@gmail.com>
2022-02-03 21:09:53 -0500

    style(view): Remove input and rows
    Remove from comunication views input and rows with trivial information

--- resources/views/communications/living.blade.php
+++ resources/views/communications/living.blade.php
@@ -24,16 +24,12 @@
                             <input type="text" class="form-control form-control-sm" name="" style="text-align:center;" value="{{ $communication->request->date_delivery }}" disabled>
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="">Monitor:</label>
-                            <input type="text" class="form-control form-control-sm" name="" style="text-align:center;" value="{{ $communication->request->requestState->name }}" disabled>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="">Comercial:</label>
+                            <label for="">Estado de solicitud:</label>
                             <input type="text" class="form-control form-control-sm" name="" style="text-align:center;" value="{{ $communication->request->requestState->name }}" disabled>
                         </div>
                         <div class="form-group col-md-4">
                             <label for="">Tipo de Servicio:</label>
-                            <input type="text" class="form-control form-control-sm" name="maquina" style="text-align:center;" value="{{ $communication->request->parametric->p_text }}" disabled>
+                            <input type="text" class="form-control form-control-sm" name="" style="text-align:center;" value="{{ $communication->request->parametric->p_text }}" disabled>
                         </div>
                     </div>
                 </div>
