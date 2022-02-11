d4d51620a7df3721de31468bc240b869475f856c
hansselhurtado <hansselhurtado@gmail.com>
2022-02-11 17:13:48 -0500

    agregando nuevos metodos a modelelos apra soportar funcionalidad

--- app/Models/ChangedRequest.php
+++ app/Models/ChangedRequest.php
@@ -44,4 +44,78 @@ class ChangedRequest extends Model
     function scopeHandleUser($query, $id){
         return $query->where('id_user', $id);
     }
+
+    // Accessor
+    public function getStateAttribute()
+    {
+        $name = null;
+        switch ($this->request_state) {
+            case 0:
+               $name = 'PENDIENTE';
+                break;
+            case 1:
+               $name = 'APROBADO';
+                break;
+            case 2:
+               $name = 'RECHAZADO';
+        }
+        return $name;
+    }
+
+    public function getValueAttribute()
+    {
+        $name = null;
+        switch ($this->request_name) {
+            case 'u_type_doc':
+               $name = Parametrics::find($this->request_value)['p_text'];
+                break;
+            case 'u_id_means':
+                $name = Parametrics::find($this->request_value)['p_text'];
+                break;
+            case 'u_id_country':
+                $name = Countries::find($this->request_value)['c_name'];
+                break;
+            default:
+                $name = $this->request_value;
+            break;
+        }
+        return $name;
+    }
+
+    public function getNameAttribute()
+    {
+        $name = null;
+        switch ($this->request_name) {
+            case 'u_name':
+               $name = 'Nombre completo';
+                break;
+            case 'u_nickname':
+               $name = 'Nombre de usuario';
+                break;
+            case 'u_key_number':
+               $name = 'Numero de telefono ';
+                break;
+            case 'u_indicativo':
+               $name = 'Indicativo de telefono ';
+                break;
+            case 'u_type_doc':
+               $name = 'Tipo de documento ';
+                break;
+            case 'u_num_doc':
+               $name = 'Numero de documento ';
+                break;
+            case 'u_id_country':
+               $name = 'Pais de origen ';
+                break;
+            case 'u_id_means':
+               $name = 'Medio de contacto ';
+                break;
+            default:
+                $name = $this->request_name;
+                break;
+        }
+        return $name;
+    }
+
+
 }
