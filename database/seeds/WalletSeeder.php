d7ab825fc5cb5afb34d8d8f02d7d9008447d422b
hansselhurtado <hansselhurtado@gmail.com>
2022-02-17 18:30:16 -0500

    se refactoriza seeder de billetra virtualñ

--- database/seeds/WalletSeeder.php
+++ database/seeds/WalletSeeder.php
@@ -5,6 +5,7 @@ use App\Models\RequestQuote;
 use App\Models\WalletDetail;
 use App\Models\WalletVirtual;
 use App\Models\Work;
+use App\Models\WorkDetail;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -17,40 +18,36 @@ class WalletSeeder extends Seeder
      */
     public function run()
     {
-        // trabajos
-        for ($i=0; $i <3; $i++) {
+        for ($i=1; $i <=3; $i++) {
+            // trabajos
             Work::create([
                 'start_date'                 => Carbon::now(),
                 'end_date'                   => Carbon::now(),
-                'request_quote_id'           => RequestQuote::all()->random()->id,
+                'request_quote_id'           => $i,
             ]);
-        }
 
-        // entregables
-        for ($i=0; $i <3; $i++) {
+            // entregables
             Deliverable::create([
                 'date_delivery'         => Carbon::now(),
                 'status'                => collect([1,2])->random(),
                 'status_deliverable'    => collect([1,2,3])->random(),
+                'work_id'               => $i,
+            ]);
+            // billetera virtual
+            WalletVirtual::create([
                 'work_id'               => Work::all()->random()->id,
             ]);
-        }
 
-        // billetera virtual
-        for ($i=0; $i <3; $i++) {
-            WalletVirtual::create([
-                'deliverable_id'        => Deliverable::all()->random()->id,
-            ]);
-        }
-
-         // detalle de la billetera virutal
-        for ($i=0; $i <3; $i++) {
             WalletDetail::create([
                 'value'                    => rand(50000, 100000),
                 'reference'                => collect(['SAB', 'XG', 'ASJ'])->random().rand(50000, 100000),
                 'wallet_virtual_id'        => WalletVirtual::all()->random()->id,
             ]);
-        }
 
+            // detalles de trabajo
+            WorkDetail::create([
+                'work_id'               => Work::all()->random()->id,
+            ]);
+        }
     }
 }
